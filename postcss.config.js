65305540973ce7f6c28195c2ea0d0f93e5253756
takayoshii <t.inagaki@jlba.club>
2020-05-19 10:49:58 +0900

    tailwind リファクタリング

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,21 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./src/**/*.tsx"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
 module.exports = {
   plugins: [
-    "postcss-import",
     "tailwindcss",
-    "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          [
+            "@fullhuman/postcss-purgecss",
+            {
+              content: [
+                "./src/pages/**/*.{js,jsx,ts,tsx}",
+                "./src/components/**/*.{js,jsx,ts,tsx}",
+              ],
+              defaultExtractor: (content) =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+            },
+          ],
+        ]
+      : []),
+    "postcss-preset-env",
   ],
 };
