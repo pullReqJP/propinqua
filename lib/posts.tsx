c4eb05d78e4df289186ccac69cfc657c1324cd09
takayoshii <t.inagaki@jlba.club>
2020-05-20 14:52:04 +0900

    mdファイル　メタデータの表示

---
+++ lib/posts.tsx
@@ -0,0 +1,39 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "src/posts");
+
+export function getSortedPostsData() {
+  // /posts 配下のファイル名を取得する
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // id を取得するためにファイル名から ".md" を削除する
+    const id = fileName.replace(/\.md$/, "");
+
+    // マークダウンファイルを文字列として読み取る
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // 投稿のメタデータ部分を解析するために gray-matter を使う
+    const matterResult = matter(fileContents);
+
+    // メタデータ部分から日付を取得
+    const date = matterResult.data.date;
+
+    // データを id と合わせる
+    return {
+      id,
+      date,
+      ...matterResult.data,
+    };
+  });
+  // 投稿を日付でソートする
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,37 @@
-import { NextPage } from "next";
+import Layout from "../components/layout";
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../../lib/posts";
 
-const Page: NextPage = () => <h1>Helloworld!</h1>;
-export default Page;
+export default function Home({ allPostsData }) {
+  console.log(allPostsData);
+  return (
+    <Layout home>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
