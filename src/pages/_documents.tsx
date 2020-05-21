9b3f4c9ba5d8a6aa1c4de3af9952cf6d16007a2d
takayoshii <t.inagaki@jlba.club>
2020-05-21 17:50:35 +0900

    awesome,google　フォント追加

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -12,6 +12,14 @@ class Document extends NextDocument<Props> {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;700&display=swap"
+          />
         </Head>
         <body>
           <Main />
