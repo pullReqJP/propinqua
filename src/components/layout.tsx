edaf2a9be868033dec55ec9bd5dbb9cccce501f3
takayoshii <t.inagaki@jlba.club>
2020-05-25 11:11:45 +0900

    Layout　スタイル調整

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default function Layout({ children }) {
   return (
-    <div className="layout">
+    <div className="layout flex flex-col">
       <header className="flex justify-around p-1 bg-gray-700 text-gray-100 font-pq">
         <div className="text-current text-3xl">pullReq</div>
       </header>
-      <main className="m-4 p-4 rounded shadow bg-white">
+      <main className="self-center md:max-w-screen-md lg:max-w-screen-lg m-4 p-4 rounded shadow bg-white">
         {children}
         <Link href="/">
           <a>← Back to home</a>
