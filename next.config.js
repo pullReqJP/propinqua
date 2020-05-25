5093116e570dee7362881bb71f3294d555d96b61
takayoshii <t.inagaki@jlba.club>
2020-05-25 12:03:26 +0900

    exportPathMap

--- next.config.js
+++ next.config.js
@@ -4,4 +4,19 @@ module.exports = {
   env: {
     assetPrefix: process.env.GITHUB_PAGES ? '/propinqua' : '',
   },
+  exportPathMap: async function () {
+    const paths = {
+      '/': { page: '/' },
+      // '/about': { page: '/about' }
+    };
+    // const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    // const data = await res.json();
+    // const shows = data.map(entry => entry.show);
+
+    // shows.forEach(show => {
+    //   paths[`/show/${show.id}`] = { page: '/show/[id]', query: { id: show.id } };
+    // });
+
+    return paths;
+  },
 };
