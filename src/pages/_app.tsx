8c7ed0b891c95f8e956b555dad91c3bcd1dabfab
takayoshii <t.inagaki@jlba.club>
2020-05-27 15:35:34 +0900

    tailwind.css 設置場所変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import './tailwind.css';
+import '../styles/tailwind.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
