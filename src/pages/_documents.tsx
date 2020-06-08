cc3a6bfcad57340497e1686a3f85b876b9105fe9
takayoshii <t.inagaki@jlba.club>
2020-06-08 13:24:02 +0900

    comfortaa　読み込み方法の変更
    ・amp化に伴い、viewport をコメントアウト

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -7,9 +7,24 @@ class Document extends NextDocument<Props> {
     return (
       <Html lang="ja">
         <Head>
-          <meta
+          {/* <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          /> */}
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/comfortaa-v28-latin-300.woff2"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/comfortaa-v28-latin-regular.woff2"
+          />
+          <link
+            rel="preload"
+            as="font"
+            href="/fonts/comfortaa-v28-latin-700.woff2"
           />
           <link rel="icon" href="/favicon.ico" />
         </Head>
