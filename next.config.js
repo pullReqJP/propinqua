e48e99c9874b0d5fdda3bc8771dd620f34147d17
takayoshii <t.inagaki@jlba.club>
2020-06-10 09:29:22 +0900

    GITHUB_PAGES　テスト

--- next.config.js
+++ next.config.js
@@ -16,26 +16,13 @@ module.exports = {
       '/product': { page: '/product' },
       '/information': { page: '/information' },
       '/sample': { page: '/sample' },
-      // '/posts': { page: '/posts' },
-      // '/posts/${id}': { page: '/posts/[id]' },
     };
-    // const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-    // const data = await res.json();
-    // const shows = data.map(entry => entry.show);
-
-    // shows.forEach(show => {
-    //   paths[`/show/${show.id}`] = { page: '/show/[id]', query: { id: show.id } };
-    // });
 
     return paths;
   },
-  // env: {
-  //   assetPrefix: process.env.NODE_ENV === 'production' ? '/propinqua' : '',
-  // },
 
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/propinqua' : '',
-
+  assetPrefix: process.env.GITHUB_PAGES ? '/propinqua' : '',
   publicRuntimeConfig: {
-    basePath: process.env.NODE_ENV === 'production' ? '/propinqua' : '',
+    basePath: process.env.GITHUB_PAGES ? '/propinqua' : '',
   },
 };
