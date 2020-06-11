fc82e6be5a639dc4675db380459320107a36a4ec
takayoshii <t.inagaki@jlba.club>
2020-06-11 17:19:53 +0900

    トップ画像　テスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,18 @@
 import Layout from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-import Date from '../components/date';
-import { getSortedPostsData } from '../../lib/posts';
-import B from '../../lib/basepath';
 
 export default function Home() {
   return (
     <Layout>
-      <div>pullReq</div>
+      <img
+        className="absolute top-0 left-0 opacity-100 transition duration-300 hover:opacity-0 ease-in-out"
+        src="/img/20200611_00.jpg"
+        alt="top_before"
+      />
+      <img
+        className="absolute top-0 left-0 opacity-0 transition duration-700 hover:opacity-100 ease-in-out"
+        src="/img/20200611_01.jpg"
+        alt="top_after"
+      />
     </Layout>
   );
 }
-// export default function Home({ allPostsData }) {
-//   // console.log(allPostsData);
-//   return (
-//     <Layout>
-//       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-//         <h2 className={utilStyles.headingLg}>Blog</h2>
-//         <ul>
-//           {allPostsData.map(({ id, date, title }) => (
-//             <li key={id}>
-//               <Link href="/posts/[id]" as={B(`/posts/${id}`)}>
-//                 <a>{title}</a>
-//               </Link>
-//               <br />
-//               <small className={utilStyles.lightText}>
-//                 <Date dateString={date} />
-//               </small>
-//             </li>
-//           ))}
-//         </ul>
-//       </section>
-//     </Layout>
-//   );
-// }
-
-// export async function getStaticProps() {
-//   const allPostsData = getSortedPostsData();
-//   return {
-//     props: {
-//       allPostsData,
-//     },
-//   };
-// }
