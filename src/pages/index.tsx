a721ec552d30fe5f44363287902d86b7e171be13
takayoshii <t.inagaki@jlba.club>
2020-06-11 17:24:35 +0900

    basepath 追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
 import Layout from '../components/layout';
+import B from '../../lib/basepath';
 
 export default function Home() {
   return (
     <Layout>
       <img
         className="absolute top-0 left-0 opacity-100 transition duration-300 hover:opacity-0 ease-in-out"
-        src="/img/20200611_00.jpg"
+        src={B('/img/20200611_00.jpg')}
         alt="top_before"
       />
       <img
         className="absolute top-0 left-0 opacity-0 transition duration-700 hover:opacity-100 ease-in-out"
-        src="/img/20200611_01.jpg"
+        src={B('/img/20200611_01.jpg')}
         alt="top_after"
       />
     </Layout>
