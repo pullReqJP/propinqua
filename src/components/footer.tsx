cda7ba3cecd3bb8965782c9cb7a606a6ee049ad4
takayoshii <t.inagaki@jlba.club>
2020-06-19 17:50:37 +0900

    フッター作成

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="apple-menu bottom-0 h-8 mt-auto py-1 pr-4 font-pq text-right">
+      privacy policy
+    </footer>
+  );
+}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,12 @@
 import Header from './header';
+import Footer from './footer';
 
 export default function Layout({ children }) {
   return (
     <div className="layout flex flex-col">
       <Header />
-      <main className="mt-8 self-center max-w-full">{children}</main>
+      <main className="my-8 self-center max-w-full">{children}</main>
+      <Footer />
     </div>
   );
 }
