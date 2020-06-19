5b9bf23f7b7d425d6658ca3629bb3f01787e8173
takayoshii <takayoshii@users.noreply.github.com>
2020-06-19 01:27:48 +0000

    deploy: d69228ef2d108d9904af28debf7f0f034b67b20e

---
+++ _next/static/KkXwJpfyf6XO7LN715KzU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/company":[a],"/contact":[a],"/hikari":[a],"/index":[a],"/posts/[id]":[a],"/product":[a],"/sample":["static\u002Fchunks\u002F05d954cf.3d352cfcc2420e535b2f.js",a],"/service":[a]}}("static\u002Fchunks\u002F04079709e086972238b5273b8febdff84cc82c5a.6fadc87338c3ad6f551d.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
