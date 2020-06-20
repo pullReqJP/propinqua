13565632f20d663201e1506cc830d4ffbdacdc6c
gackey.21 <gackey.21@gmail.com>
2020-06-20 12:01:07 +0900

    Warning 対処

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,9 @@ import B from '../../lib/basepath';
 export default function Footer() {
   return (
     <footer className="apple-menu bottom-0 h-8 mt-auto py-1 pr-4 font-pq text-right">
-      <Link href={B('/privacy_policy')}>privacy policy</Link>
+      <Link href={B('/privacy_policy')}>
+        <a>privacy policy</a>
+      </Link>
     </footer>
   );
 }
