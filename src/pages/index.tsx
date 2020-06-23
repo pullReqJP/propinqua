5e4c22ec81d8a118999b1651988d027d9b606408
takayoshii <t.inagaki@jlba.club>
2020-06-23 11:16:10 +0900

    meta タグ追加テスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
+import Head from 'next/head';
 import Layout from '../components/layout';
 import B from '../../lib/basepath';
 
 export default function Home() {
   return (
-    <Layout>
-      <img
-        className="absolute top-0 left-0 opacity-100 transition duration-300 hover:opacity-0 ease-in-out"
-        src={B('/img/20200611_00.jpg')}
-        alt="top_before"
-      />
-      <img
-        className="absolute top-0 left-0 opacity-0 transition duration-700 hover:opacity-100 ease-in-out"
-        src={B('/img/20200611_01.jpg')}
-        alt="top_after"
-      />
-    </Layout>
+    <>
+      <Head>
+        <title>pullReq</title>
+        <meta name="description" content="pullReq" />
+      </Head>
+      <Layout>
+        <img
+          className="absolute top-0 left-0 opacity-100 transition duration-300 hover:opacity-0 ease-in-out"
+          src={B('/img/20200611_00.jpg')}
+          alt="top_before"
+        />
+        <img
+          className="absolute top-0 left-0 opacity-0 transition duration-700 hover:opacity-100 ease-in-out"
+          src={B('/img/20200611_01.jpg')}
+          alt="top_after"
+        />
+      </Layout>
+    </>
   );
 }
