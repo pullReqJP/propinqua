d259c96186fbb80cc70fa8a9b96d19fde70354ba
takayoshii <t.inagaki@jlba.club>
2020-06-24 16:54:48 +0900

    GA_TRACKING_ID　修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,7 +22,7 @@ class MyDocument extends Document<Props> {
   // }
 
   render() {
-    const GA_TRACKING_ID = 'UA-170600015-1';
+    const GA_TRACKING_ID = 'GTM-TGT2JV7';
     const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GA_TRACKING_ID}');`;
     const gtmFrame = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
 
