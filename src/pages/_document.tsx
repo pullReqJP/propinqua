1a5877c339032330b74f7e9dfd5c0adbf4a9d95e
takayoshii <t.inagaki@jlba.club>
2020-07-03 14:29:46 +0900

    ページ名を body の class に付加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,6 +36,12 @@ class MyDocument extends Document<Props> {
   // }
 
   render() {
+    const url = this.props.__NEXT_DATA__.page;
+    const pageName = url.replace(/(?<!^)\/.*/, '').replace('/', '');
+
+    const query = this.props.__NEXT_DATA__.query;
+    const pageID = query.id ? ' ' + query.id : '';
+
     const GA_TRACKING_ID = 'GTM-TGT2JV7';
     const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GA_TRACKING_ID}');`;
     const gtmFrame = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
@@ -66,7 +72,7 @@ class MyDocument extends Document<Props> {
           />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <body>
+        <body className={pageName + pageID}>
           <noscript dangerouslySetInnerHTML={{ __html: gtmFrame }} />
           <Main />
           <NextScript />
