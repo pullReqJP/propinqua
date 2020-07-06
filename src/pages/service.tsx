864f7bcc297c3b1f369156d4a769c25151afc498
takayoshii <t.inagaki@jlba.club>
2020-07-06 16:24:49 +0900

    サービス　スタイル調整

--- src/pages/service.tsx
+++ src/pages/service.tsx
@@ -1,37 +1,60 @@
 import Head from 'next/head';
 import Layout from '../components/layout';
+import ServiceSymbol from 'react-svg-loader!../components/service.svg';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+
+const Para = styled('p')`
+  ${tw`my-6`};
+  text-indent: 1em;
+`;
 
 export default function service() {
   return (
     <>
       <Head>
-        <title>商品 | 株式会社プルリク</title>
+        <title>サービス概要 | 株式会社プルリク</title>
         <meta name="description" content="Pullreq Limited" />
       </Head>
 
       <Layout>
         <div className="container p-4 break-words">
-          <h1>商品</h1>
-          <p className="mt-6">
-            ○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○
-          </p>
-          <h2 id="hp">HPの作成・リニューアル</h2>
-          <p className="mt-6">
-            企画開発、保守、ドメイン取得、SEO対策、サーバーなどWEBページを作成するのに必要なものはすべてワンストップ化
-          </p>
-          <p className="mt-6">
-            また、EC販売やWEBページに連動した社内業務効率化を図るソフト開発などもお任せください。
-          </p>
-          <h2 id="system">システム受託開発</h2>
-          <p className="mt-6">
-            テレワークに必要な環境の構築から、受注管理システム、勤怠管理や、給与計算など、パッケージソフトでは叶わない、お客様の細かなニーズに合わせたソフトウェアの開発など、幅広く対応いたします。
-          </p>
-          <h2 id="hikari">「pqひかり」光サービス</h2>
-          <p className="mt-6">
-            1ギガ、10ギガタイプにそれぞれ、速度制限にかからない「no
-            limit」プランをご用意し、お客様に合った形でハイクオリティな光回線を提供いたします。
-          </p>
-          <p className="mt-6">Coming soon...</p>
+          <h1>サービス概要</h1>
+          <div className="grid md:grid-cols-2 col-gap-8">
+            <ServiceSymbol
+              className="
+                w-full
+                lg:h-full
+                border-4
+                rounded-4rem
+                border-black
+                border-opacity-60
+                lg:row-span-3
+              "
+            />
+            <div>
+              <h2 id="hp">HPの作成・リニューアル</h2>
+              <Para>
+                企画開発、保守、ドメイン取得、SEO対策、サーバーなどWEBページを作成するのに必要なものはすべてワンストップ化
+              </Para>
+              <Para>
+                また、EC販売やWEBページに連動した社内業務効率化を図るソフト開発などもお任せください。
+              </Para>
+            </div>
+            <div>
+              <h2 id="system">システム受託開発</h2>
+              <Para>
+                テレワークに必要な環境の構築から、受注管理システム、勤怠管理や、給与計算など、パッケージソフトでは叶わない、お客様の細かなニーズに合わせたソフトウェアの開発など、幅広く対応いたします。
+              </Para>
+            </div>
+            <div>
+              <h2 id="hikari">「pqひかり」光サービス</h2>
+              <Para>
+                1ギガ、10ギガタイプにそれぞれ、速度制限にかからない「no
+                limit」プランをご用意し、お客様に合った形でハイクオリティな光回線を提供いたします。
+              </Para>
+            </div>
+          </div>
         </div>
       </Layout>
     </>
